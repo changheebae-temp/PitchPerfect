d1b484b6972f5fd0493fe231d7bd2b7ed81bed7e
Changhee Bae <changhee.bae.ai@gmail.com>
2018-03-18 20:25:46 +0900

    first project on Udacity iOS devleoper Nanodegree program

---
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -0,0 +1,78 @@
+//
+//  RecordSoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Changhee Bae on 11/03/2018.
+//  Copyright © 2018 Changhee Bae. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    var audioRecorder: AVAudioRecorder!
+
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
+    }
+    
+    // MARK: IBActions
+
+    @IBAction func recordAudio(_ sender: UIButton) {
+        configureRecordingUI(isRecording: true)
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+    
+    @IBAction func stopRecording(_ sender: UIButton) {
+        configureRecordingUI(isRecording: false)
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+    
+    // MARK: Audio Recorder Delagate
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        } else {
+            print("recording was not successful")
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
+    
+    // MARK: UI Functions
+    
+    func configureRecordingUI(isRecording: Bool) {
+        recordingLabel.text = (isRecording ? "Recording in Progress" : "Tap to Record")
+        stopRecordingButton.isEnabled = (isRecording ? true : false)
+        recordingButton.isEnabled = (isRecording ? false : true)
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# App Demo
+A simple AVFoundation app which uses user's iPhone microphone to record audio and apply audio effects (slow, fast, high, low, echo, and reverb) on the recording.
+![PitchPerfectAppDemo](https://github.com/changheebae/PitchPerfect/blob/master/READMEImage/PitchPerfectAppDemo.png)
